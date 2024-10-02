8a44fac4b8c26d702abd4dd0178b9e0deac58e2c
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-10-02 09:20:46 -0300

    Add files via upload

--- 2024-3-D.S-tratamento-de-dados-main/graficos/graficoBarra.js
+++ 2024-3-D.S-tratamento-de-dados-main/graficos/graficoBarra.js
@@ -1,10 +1,13 @@
 
 async function criarGraficoBarra(){
-    const url= 'https://raw.githubusercontent.com/SamuelVn007/2024-3-D.S-tratamento-de-dados/refs/heads/main/salgados.json'
+    const url= 'https://raw.githubusercontent.com/SamuelVn007/2024-3-D.S-tratamento-de-dados/refs/heads/main/lista.json'
     const res = await fetch(url)
     const dados = await res.json()
+    const nome = Object.keys(dados)
+    const quantidade = Object.values(dados)
 
-    console.log(dados)
+    console.log(Object.keys(dados))
+
 }
 
 criarGraficoBarra()
