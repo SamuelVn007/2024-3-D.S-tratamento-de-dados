aed5e714bfdff93b59093ed964cd7d5e6eb8e078
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-10-02 12:05:29 -0300

    Add files via upload

---
+++ 2024-3-D.S-tratamento-de-dados-main/graficos/graficoPizza.js
@@ -0,0 +1,8 @@
+async function criarGraficoPizza(){
+    const url = 'https://raw.githubusercontent.com/SamuelVn007/2024-3-D.S-tratamento-de-dados/refs/heads/main/listaEscola.json'
+    const res = await fetch(url)
+
+    for(let i = 0; i <= dados.length; i++){
+        
+    }
+}
